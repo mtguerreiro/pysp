c02a7c6a336d6c41988787cc1b81aaf3aeca3c01
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2019-03-07 16:16:27 -0300

    Added naive IDFT, review of DFT example

--- examples/dft_example.py
+++ examples/dft_example.py
@@ -5,15 +5,15 @@ import time
 
 # --- Input ---
 f = 100
-fs = 100e3
+fs = 10e3
 
 #N = 3*int(fs/f)
-N = 2**12
+N = 2**10
 
 # --- Signal ---
 t = (1/fs)*np.arange(N)
 
-x = np.sin(300*2*np.pi*f*t)
+x = np.sin(2*np.pi*f*t)
 
 # --- DFT ---
 Nx = int(N/2)
@@ -22,14 +22,28 @@ fx = (fs/N)*np.arange(Nx)
 _ti = time.time()
 X = pysp.ft.dft_naive(x)
 _tf = time.time()
-print(_tf - _ti)
 
 Xm = np.abs(X[:Nx])
 
 # --- Output ---
+# Plots
 plt.ion()
-plt.figure()
-plt.plot(t, x)
+plt.figure(figsize=(12, 5))
 
-plt.figure()
-plt.plot(fx, Xm)
+plt.subplot(1, 2, 1)
+plt.plot(t/1e-3, x)
+plt.grid()
+plt.title('Time domain')
+plt.xlabel('Time (ms)')
+plt.ylabel('Amplitude')
+
+plt.subplot(1, 2, 2)
+plt.plot(fx/1e3, Xm)
+plt.grid()
+plt.title('Frequency domain')
+plt.xlabel('Frequency (kHz)')
+plt.ylabel('Amplitude')
+
+# Prints
+print('Number of points:', N)
+print('Execution time:', _tf - _ti)
--- pysp/ft.py
+++ pysp/ft.py
@@ -3,12 +3,27 @@ import numpy as np
 def dft_naive(x):
 
     N = x.shape[0]
+
     n = np.arange(N)
     k = np.arange(N).reshape(-1, 1)
 
     cos = np.cos(2*np.pi*k*n/N)
     sin = np.sin(2*np.pi*k*n/N)
     
-    X = x*(cos - 1j*sin)
+    X = (cos - 1j*sin) @ x
 
-    return np.sum(X, 1)
+    return X
+
+def idft_naive(X):
+
+    N = X.shape[0]
+
+    n = np.arange(N).reshape(-1, 1)
+    k = np.arange(N)
+
+    cos = np.cos(2*np.pi*k*n/N)
+    sin = np.sin(2*np.pi*k*n/N)
+
+    x = (cos + 1j*sin) @ X
+
+    return x/N
