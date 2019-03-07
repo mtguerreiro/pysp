cfcb0435d7b3d12c63cfaef64988f7141c1a74ef
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2019-03-07 16:29:13 -0300

    Added IDFT example, reviwed DFT example

--- examples/idft_example.py
+++ examples/idft_example.py
@@ -4,8 +4,8 @@ import pysp
 import time
 
 # --- Input ---
-# Signal's frequency
-f = 100
+# Number of frequency components
+Nf = 1
 
 # Sampling frequency
 fs = 10e3
@@ -14,19 +14,22 @@ fs = 10e3
 N = 2**10
 
 # --- Signal ---
+Nx = int(N/2)
+fidx = np.random.randint(0, Nx/10, Nf)
+
+X = np.zeros(N, dtype=complex)
+X[fidx] = np.random.randint(-10, 10, Nf) + 1j*np.random.randint(-10, 10, Nf)
+X[-fidx] = X[fidx]
+
+fx = (fs/N)*np.arange(Nx)
+Xm = np.abs(X[:Nx])
+
+# --- IDFT ---
 t = (1/fs)*np.arange(N)
 
-x = np.sin(2*np.pi*f*t)
-
-# --- DFT ---
-Nx = int(N/2)
-fx = (fs/N)*np.arange(Nx)
-
 _ti = time.time()
-X = pysp.ft.dft_naive(x)
+x = pysp.ft.dft_naive(X)
 _tf = time.time()
-
-Xm = np.abs(X[:Nx])
 
 # --- Output ---
 # Plots
@@ -34,17 +37,17 @@ plt.ion()
 plt.figure(figsize=(12, 5))
 
 plt.subplot(1, 2, 1)
-plt.plot(t/1e-3, x)
-plt.grid()
-plt.title('Time domain')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-
-plt.subplot(1, 2, 2)
 plt.plot(fx/1e3, Xm)
 plt.grid()
 plt.title('Frequency domain')
 plt.xlabel('Frequency (kHz)')
+plt.ylabel('Amplitude')
+
+plt.subplot(1, 2, 2)
+plt.plot(t/1e-3, x.real)
+plt.grid()
+plt.title('Time domain')
+plt.xlabel('Time (ms)')
 plt.ylabel('Amplitude')
 
 # Prints
