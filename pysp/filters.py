4b28ea9709edb243f64929890bfe23800e7a145a
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2019-05-01 15:13:33 -0300

    Improvements in butter

--- pysp/filters.py
+++ pysp/filters.py
@@ -1,9 +1,11 @@
 import numpy as np
+import scipy.signal
 
-
-def butter(wp, Hwp, ws, Hws, cutoff='wp', show_partial=False):
+def butter(wp, Hwp, ws, Hws, cutoff='wp', T=1, show_partial=False):
     """Determines the order and cut-off frequency for a Butterworth filter,
     based on the specifications of pass-band/stop-band frequency/attenuation.
+
+    Continue with docs...
 
     Parameters
     ----------
@@ -22,16 +24,13 @@ def butter(wp, Hwp, ws, Hws, cutoff='wp', show_partial=False):
         frequency.
         
     """
-    A = lambda Hw : 1/Hw**2 - 1
+    A = lambda H_w : 1/H_w**2 - 1
 
-    # --- Corner frequency and order ---
     # Corner frequency
-    log_wc = np.log10(A(Hwp))*np.log10(ws) - np.log10(A(Hws))*np.log10(wp)
-    log_wc = log_wc/(np.log10(A(Hwp)/A(Hws)))
-    _wc = 10**log_wc
+    _wc = butter_corner(wp, Hwp, ws, Hws, cutoff)
 
     # Order
-    _N = np.log10(A(Hwp))/(2*np.log10(wp/_wc))
+    _N = butter_order(_wc, wp, Hwp)
     
     # Actual order, cut-off
     N = np.ceil(_N).astype(int)
@@ -39,22 +38,74 @@ def butter(wp, Hwp, ws, Hws, cutoff='wp', show_partial=False):
         wc = wp/(A(Hwp)**(1/2/N))
     else:
         wc = ws/(A(Hws)**(1/2/N))
+        
+    # Poles
+    sk = butter_poles(wc, N)
 
-    if show_partial is True:
-        print('Order: ', _N)
-        print('Cut-off: ', _wc)
-
-    # --- Poles ----
-    k = np.arange(2*N)
-    sk = wc*np.exp((1j*np.pi/2/N)*(2*k + N - 1))
-
-    if show_partial is True:
-        print('Poles:')
-        print(sk)
+    butter_tf(sk, T)
 
     butter_filter = {}
     butter_filter['wc'] = wc
     butter_filter['N'] = N
     butter_filter['poles'] = sk
-    
+
+    # Printing
+    if show_partial is True:
+        print('Order: ', _N)
+        print('Cut-off: ', _wc)
+        
+        print('Poles:')
+        print(sk)
+
     return butter_filter
+
+
+def butter_corner(wp, Hwp, ws, Hws, cutoff='wp'):
+
+    A = lambda H_w : 1/H_w**2 - 1
+    
+    log_wc = np.log10(A(Hwp))*np.log10(ws) - np.log10(A(Hws))*np.log10(wp)
+    log_wc = log_wc/(np.log10(A(Hwp)/A(Hws)))
+
+    return 10**log_wc
+
+
+def butter_order(wc, w, Hw):
+
+    A = lambda H_w : 1/H_w**2 - 1
+
+    return np.log10(A(Hw))/(2*np.log10(w/wc))
+
+
+def butter_poles(wc, N):
+
+    k = np.arange(2*N)
+    return wc*np.exp((1j*np.pi/2/N)*(2*k + N - 1))
+
+
+def butter_tf(poles, T):
+
+    sk_poly = np.poly(poles[poles.real < 0])
+    
+    Ak, pk, k = scipy.signal.residue(sk_poly[-1], sk_poly)
+    pk_sort_idx = np.argsort(pk)
+    Ak = Ak[pk_sort_idx]
+    pk = pk[pk_sort_idx]
+
+    k = 0
+    while k < len(Ak):
+
+        if np.abs(np.imag(pk[k])) > 1e-10:
+            a1 = np.poly(1/np.exp(T*-pk[k]))
+            a2 = np.poly(1/np.exp(T*-pk[k+1]))
+            a = np.polymul(a1, a2)
+            A = T*Ak[k]*a2 + T*Ak[k+1]*a1
+
+            print('Den: ')
+            print(a.real)
+            print('Num: ')
+            print(A.real)
+
+            k+=1
+
+        k+=1
